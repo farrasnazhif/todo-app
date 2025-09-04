c0fcca4b33f0f1289fa8e1bceb20e8cef8a970fc
farrasnazhif <farrasnazhif@gmail.com>
2025-09-04 16:53:41 +0700

    remove dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,6 @@ import Navbar from "./fragments/Navbar";
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [darkMode, setDarkMode] = useState(() => {
-    const savedDarkMode = JSON.parse(localStorage.getItem("darkMode"));
-    return savedDarkMode || false;
-  });
 
   useEffect(() => {
     const savedNotes = JSON.parse(localStorage.getItem("notes"));
@@ -25,40 +21,10 @@ function App() {
     }
   }, [notes]);
 
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark-mode", darkMode);
-    document.documentElement.classList.toggle("light-mode", !darkMode);
-
-    localStorage.setItem("darkMode", JSON.stringify(darkMode));
-  }, [darkMode]);
-
-  function addNote(newNote) {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
-  }
-
-  function handleDeleteNote(id) {
-    setNotes((prevNotes) => prevNotes.filter((_, index) => index !== id));
-  }
-
   return (
-    <div className={`app-wrapper ${darkMode ? "dark-mode" : "light-mode"}`}>
-      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
-      <main className="main-content">
-        <div className="create-area-container">
-          <CreateArea onAdd={addNote} />
-        </div>
-        <div className="note-container">
-          {notes.map((note, index) => (
-            <Note
-              key={index}
-              id={index}
-              title={note.title}
-              content={note.content}
-              onDelete={handleDeleteNote}
-            />
-          ))}
-        </div>
-      </main>
+    <div className={`app-wrapper `}>
+      <Navbar />
+
       <Footer />
     </div>
   );
